72ae296c476c2e421619f5db835460ec5a07863f
MridulShahh <mriduldevshah@gmail.com>
2026-06-17 21:35:49 +0530

    Day 2: Product model, controller, routes - CRUD APIs complete

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./src/config/db");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/products", require("./src/routes/productRoutes"));
+
+app.get("/", (req, res) => {
+  res.send("Smart Cart Backend Running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
